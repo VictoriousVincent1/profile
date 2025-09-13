cfbcff6c6a8f697499f29fd1445e23abc85b0344
Vincent <vincentwu2810@gmail.com>
2025-09-13 17:54:42 -0500

    Update all project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 tailwind.config = {
   theme: {
-    extend: {},
+    gridTemplateColumns: {
+      'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+      
+    },
+    fontFamily: {
+      Outfit: ['Outfit', "sans-serif"],
+      Ovo: ['Ovo', "serif"]
+    },
+    boxShadow:  {
+      'black': '4px 4px 0px 0px #000',
+      'white': '4px 4px 0px 0px #fff'
+    },
+    colors:{
+      darkTheme: '#11001F'
+    }
+
   },
-  plugins: [],
+  darkMode: 'selector'
 }
